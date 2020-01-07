0f3fde37db27788ea179c8072ee7acdb78417833
StefanvdBerg00 <stefanvdberg2000@gmail.com>
2020-01-07 13:10:10 +0100

    visualisatie2

--- code/visualisation.py
+++ code/visualisation.py
@@ -7,14 +7,14 @@ def visualisation(connections, quality):
 
     for connection in connections:
         plt.plot([connection.city1.y, connection.city2.y], [connection.city1.x, connection.city2.x], "red", zorder=1)
-        plt.annotate(connection.city1.name, (connection.city1.y, connection.city1.x))
-        plt.annotate(connection.city2.name, (connection.city2.y, connection.city2.x))
-        plt.scatter([connection.city1.y, connection.city2.y], [connection.city1.x, connection.city2.x], color="blue", zorder=2)
-        mplcursors.cursor(plt.scatter([connection.city1.y, connection.city2.y], [connection.city1.x, connection.city2.x], color="blue", zorder=2))
+        # plt.annotate(connection.city1.name, (connection.city1.y, connection.city1.x))
+        # plt.annotate(connection.city2.name, (connection.city2.y, connection.city2.x))
+        plt.scatter([connection.city1.y, connection.city2.y], [connection.city1.x, connection.city2.x], color="blue", zorder=2, label=[connection.city1.name, connection.city2.name])
 
     text = "K:     " + str(quality["K"]) + "\np:     " + str(quality["p"]) + "\nT:     " + str(quality["T"]) + "\nMin: " + str(quality["Min"])
 
     plt.text(3.5, 52.95, text, bbox=dict(facecolor='beige', edgecolor='black'))
     plt.xlim(3.355, 7.222)
     plt.ylim(50.703, 53.522)
+    mplcursors.cursor()
     plt.show()
