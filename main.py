bec7e347ce2b7e5862d76b1c23580c78a74e5b7e
StefanvdBerg00 <stefanvdberg2000@gmail.com>
2020-01-07 11:43:45 +0100

    visualisatie

--- main.py
+++ main.py
@@ -1,56 +1,36 @@
-import csv
+from visualisation import visualisation
+from classes import City, Connection
+from csvdata import csvdata
 
-class City:
-    def __init__(self, name):
-        self.name = name
-        self.connections = []
-
-    def add(self, connection):
-        self.connections.append(connection)
-
-    def __str__(self):
-        return f"{self.name}"
-
-class Connection:
-    def __init__(self, city1, city2, time):
-        self.city1 = city1
-        self.city2 = city2
-        self.time = time
-        self.visited = False
-
-    def __str__(self):
-        return f"From {self.city1} to {self.city2} ({self.time} min)"
-
-cities = {}
+csvdata = csvdata()
+cities = csvdata["cities"]
+all_connections = csvdata["all_connections"]
 max_length = 0
 
-with open("data/ConnectiesHolland.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        city1 = row[0]
-        city2 = row[1]
-        time = int(row[2])
+def main_city(cities):
+    max_length = 0
 
-        if city1 not in cities:
-            cities[city1] = City(city1)
-        if city2 not in cities:
-            cities[city2] = City(city2)
+    for city in cities:
+        length = len([connection for connection in cities[city].connections if not connection.visited])
+        if length > max_length:
+            max_length = length
+            current_city = cities[city]
 
-        connection = Connection(cities[city1], cities[city2], time)
-        cities[city1].add(connection)
-        cities[city2].add(connection)
+    return current_city
 
-        if len(cities[city1].connections) > max_length:
-            max_length = len(cities[city1].connections)
-            current_city = cities[city1]
-        if len(cities[city2].connections) > max_length:
-            max_length = len(cities[city2].connections)
-            current_city = cities[city2]
+def quality(trajects):
+    T = len(trajects)
+    Min = sum([traject["time"] for traject in trajects])
+    p = len([connection for connection in all_connections if connection.visited]) / len(all_connections)
+    print(f"T: {T}, Min: {Min}, p: {p}")
+    return p*10000 - (T*100 + Min)
+
 
 trajects = []
 
 found = True
 while found:
+    current_city = main_city(cities)
     start_city = current_city
     found = False
     time = 0
@@ -61,7 +41,7 @@ while found:
         next = False
         prev_city = current_city
         for connection in current_city.connections:
-            if connection not in route and (time + connection.time < 120) and not connection.visited:
+            if connection not in route and (time + connection.time < 180) and not connection.visited:
                 route.append(connection)
                 connection.visited = True
                 time += connection.time
@@ -71,12 +51,13 @@ while found:
 
         if not next:
             if current_city == prev_city:
-                if change:
-                    current_city = start_city
-                    route.reverse()
-                    change = False
-                else:
+                if not change:
                     break
+
+                current_city = start_city
+                route.reverse()
+                change = False
+
 
     trajects.append({"route":route, "time": time})
 
@@ -87,4 +68,9 @@ while found:
                 found = True
                 break
 
-    print([f"{connection}" for connection in route])
+    # print(trajects[-1]["time"])
+    # print([f"{connection}" for connection in route])
+print(f"Aantal trajecten: {len(trajects)}")
+print(f"Functie: {quality(trajects)}")
+
+visualisation(all_connections)
