282d2abfc54e9364d4570cc187054b94eb2bbf7b
StefanvdBerg00 <stefanvdberg2000@gmail.com>
2020-01-23 15:24:53 +0100

    opschonen

--- main.py
+++ main.py
@@ -9,8 +9,10 @@
 
 import os, sys
 directory = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "algorithms"))
+sys.path.append(os.path.join(directory, "code", "classes"))
+sys.path.append(os.path.join(directory, "code", "input"))
+sys.path.append(os.path.join(directory, "code", "output"))
 
 from output import run, load
 from visualisation import visualisation
